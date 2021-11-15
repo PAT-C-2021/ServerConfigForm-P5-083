8e580068ba42c2a76708e95e5639299ead9331e3
Yusuf Zidane Rukmanda <yusuf.zidane.ft19@mail.umy.ac.id>
2021-11-15 15:36:07 +0700

    Add project files.

---
+++ ServerConfigForm-P5-20190140083/Form1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ServiceMtk_P1_20190140083;
+
+
+namespace ServerConfigForm_P5_20190140083
+{
+    public partial class Form1 : Form
+    {
+        ServiceHost Host = new ServiceHost(typeof(Matematika));
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOn_Click(object sender, EventArgs e)
+        {
+            Host = new ServiceHost(typeof(Matematika));
+
+            labelOnOrOff.Text = "Server ON";
+            labelKet.Text = "Klik OFF untuk mematikan Server";
+            Host.Close();
+            buttonOn.Enabled = false;
+            buttonOff.Enabled = true;
+
+        }
+
+        private void buttonOff_Click(object sender, EventArgs e)
+        {
+            buttonOn.Enabled = true;
+            buttonOff.Enabled = false;
+            Host.Close();
+            labelKet.Text = "Klik ON untuk Menjalankan Server";
+            labelOnOrOff.Text = "Server OFF";
+        }
+    }
+}
